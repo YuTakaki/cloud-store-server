92def1c759f6fb6537ca3cdf1c05b35e538effd7
yutakaki <yu.takaki2000@gmail.com>
2022-06-04 22:45:17 +0900

    add error handling in register controller

--- src/api/auth/controller.ts
+++ src/api/auth/controller.ts
@@ -1,4 +1,4 @@
-import { Request, Response, Router } from "express";
+import e, { Request, Response, Router } from "express";
 import { AuthManager } from "./manager";
 import bcrypt from "bcrypt";
 import { BaseController } from "../common/controller";
@@ -26,32 +26,55 @@ export class AuthController implements BaseController{
 
   private register = async(req : Request, res : Response) => {
     try {
+
+      type error = {
+        username? : string,
+        email?: string
+      }
+
+      const errors : error = {}
       const {
-        password
+        password,
+        username,
+        email
       } = req.body;
       const salt = await bcrypt.genSalt(10);
-      const hash = await bcrypt.hash(password, salt)
+      const hash = await bcrypt.hash(password, salt);
 
-      const saved_user = await this.manager.saveUser({
-        ...req.body,
-        password: hash
-      });
+      const userByUsername = await this.manager.findUser(username);
+      const userByEmail = await this.manager.findUser(email);
 
-      const user = await this.manager.findUser(saved_user.username);
+      if (userByUsername) {
+        errors.username = "username already exist"
+      }
 
-      const token = generateToken(saved_user.id);
-      return res.cookie("token", token, {
-        httpOnly: true,
-        sameSite: 'none',
-        secure: true,
-      }).send({
-        user,
-        token,
-      })
-      
+      if (userByEmail) {
+        errors.email = "email already exist"
+      }
+
+      if (Object.keys(errors).length === 0) {
+        const saved_user = await this.manager.saveUser({
+          ...req.body,
+          password: hash
+        });
+        
+        const user = await this.manager.findUser(saved_user.username);
+  
+        const token = generateToken(saved_user.id);
+        return res.cookie("token", token, {
+          httpOnly: true,
+          sameSite: 'none',
+          secure: true,
+        }).send({
+          user,
+          token,
+        })
+      } else {
+        return res.status(400).send(errors)
+      }
     } catch (error) {
       console.log(error);
-      return res.send(error)
+      return res.status(400).send(error)
     }
   }
 
